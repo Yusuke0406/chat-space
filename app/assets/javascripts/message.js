f6dea0aa8a4684384d68ae7b9e8d933b042457d3
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2019-11-13 18:45:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-     var image = message.image? `<img src = "${message.image}"></img>` : "";
+     var image = message.image? `<img src = "${message.image}"></img>` :"";
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
